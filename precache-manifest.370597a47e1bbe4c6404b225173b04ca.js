8c621863ddc5bbb246dba99245bce34b9416af8e
root <root@Will-PC.localdomain>
2020-09-21 14:11:13 +1200

    Updates

---
+++ precache-manifest.370597a47e1bbe4c6404b225173b04ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd04f954a5f8fb200481d325738ab19",
+    "url": "/2048-react/index.html"
+  },
+  {
+    "revision": "7276f78c3df7699da1ed",
+    "url": "/2048-react/static/css/main.182b2e12.chunk.css"
+  },
+  {
+    "revision": "35db8830c3de3c6b5144",
+    "url": "/2048-react/static/js/2.3092a3b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048-react/static/js/2.3092a3b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7276f78c3df7699da1ed",
+    "url": "/2048-react/static/js/main.9f971970.chunk.js"
+  },
+  {
+    "revision": "a0a334956046092609eb",
+    "url": "/2048-react/static/js/runtime-main.cde21f69.js"
+  }
+]);
